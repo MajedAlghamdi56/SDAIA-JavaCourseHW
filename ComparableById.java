a6dadf15e3741cda02d42341675e6bc5f8a9fc31
Majed Alghamdi <109752818+MajedAlghamdi56@users.noreply.github.com>
2024-06-01 20:56:04 +0300

    Add files via upload

---
+++ ComparableById.java
@@ -0,0 +1,5 @@
+package Day3HW;
+
+public interface ComparableById {
+    boolean compareById(int id);
+}
---
+++ Main.java
@@ -0,0 +1,17 @@
+package Day3HW;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person("Abdullah", 17, "100 King-AbdulAziz St");
+        Person person2 = new Person("Majed", 25, "200 PrinceS St");
+        Person person3 = new Person("Faisal", 23, "300 KingABD St");
+        Person person4 = new Person("Mohammed", 21, "400 PrinceM Rd");
+
+        System.out.println(person1);
+        System.out.println(person2);
+        System.out.println(person3);
+        System.out.println(person4);
+    }
+
+}
+
